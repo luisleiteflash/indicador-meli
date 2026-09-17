8b8413e239658866865089236f03bf0cb6e82c72
luisleiteflash <luis.leite@move3.com.br>
2026-09-17 10:25:17 -0300

    Atualiza dados.js - 17/09/2026 10:25

--- dados.js
+++ dados.js
@@ -1,6 +1,6 @@
 /* gerado automaticamente a partir da planilha - nao editar a mao */
 window.MELI = {
-  atualizado: "16/09/2026 16:00",
+  atualizado: "17/09/2026 10:24",
   arquivo: "INDICADORES SEMANAIS E MENSAIS 1Q - AGOSTO (1) (1).xlsx",
   ano: 2026,
   rotas: {
